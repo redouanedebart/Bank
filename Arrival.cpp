a35251bcd0bec53a092c5d2c63822442c9fe9c87
msauvage <47172455+msauvage@users.noreply.github.com>
2022-09-19 13:23:54 +0200

    Update void Arrival::handle dans Arrival.cpp
    J'ai fait un parcours de la liste des caissiers présents dans la Bank
    Pour chaque caissier dans la liste, je vérifie s'il est libre:
    - S'il est LIBRE:
      - J'incrémente le nombre de clients servis par le caissier jusqu'à présent
      - Le caissier devient "pas libre"
      - ...
      Resterait à voir s'il faut-il modifier le "_serviceTime" (mean meanServiceDuration)
      - Je mets à jour la liste (en modifiant le caissier dans la liste à la place où l'on avait trouvé le caissier libre)
      - A la fin, j'arrête de parcourir la liste des caissiers
    
    - S'il n'est PAS LIBRE:
      J'ajoute le client qui arrive dans la file d'attente (à revoir)

--- Arrival.cpp
+++ Arrival.cpp
@@ -20,4 +20,50 @@ Arrival::~Arrival() {}
  * If any Cashier is available, the client goes to the first Cashier available,
  * else, the client is added to the queue
  */
-void Arrival::handle() {}
+void Arrival::handle() {
+  
+  // MODIFICATION APPORTEES PAR MATTHIEU SAUVAGEOT (19/09/2022)
+  
+  // Parcours de la liste _cashierList (voir Bank.h)
+  for(i=0; i< _cashierList.size();i++){
+    
+    // Récupération du caissier i
+    Cashier cashier_i = _cashierList[i];
+    
+    // Vérifier si le caissier est libre
+    if(cashier_i.isFree()){
+      // Le caissier en question commence à s'occuper du client
+      // ...
+      
+      // On récupère le nombre de clients servis par le caissier jusqu'à présent
+      int nbClients = caissier_i.clientCount();
+      
+      // Incrémentation du nombre de clients servis par le caissier
+      nbClients+=1;
+      
+      // Attribution de la nouvelle valeur pour le nombre de clients servis par le caissier
+      cashier_i._clientCounter = nbClients;
+      
+      // Le caissier n'est plus libre
+      cashier_i._free = 'false';
+      
+      // QUESTION: Faut-il modifier le "_serviceTime" (mean meanServiceDuration)
+      
+      // ...
+      
+      // Mise à jour de la liste (modification du caissier i)
+      cashierList[i] = cashier_i;
+      
+      // Après avoir modifier le caissier i, on sort de la boucle "for"
+      exit 0;
+      
+    }
+    // S'il n'y a pas de caissier libre
+    // Il faut ajouter le client à la File d'Attente (WaitingQueue)
+    
+    // QUESTION: Où se trouve la File D'attente (ex: dans Bank.cpp, Client.cpp, Caishier.cpp, ...)?
+    // A REVOIR (pour "queue" et "client")
+    // queue.addClient(client)
+    
+  }
+}
