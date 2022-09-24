f36056d9fa38eea010e37f25c78295f5d5545270
msauvage <47172455+msauvage@users.noreply.github.com>
2022-09-24 08:54:40 +0200

    Update void Departure::handle() in Departure.cpp
    On considère la file d'attente "queue" qui est récupérée via une nouvelle fonction dans Bank.cpp.
    
    Si cette file d'attente est vide, le caissier attend. 
    Sinon, le premier client qui arrive est servi par le caissier.
    
    Remarque: Pour que le caissier commence à s'occuper du 1er client arrivé, je pense qu'il faudra utiliser la fonction "handle()" de Arrival.cpp, quand elle sera prête.

--- Departure.cpp
+++ Departure.cpp
@@ -8,4 +8,31 @@ Departure::~Departure() {}
 /**
  * if the waiting queue is empty, the cashier waits, else, the first client is served
  */
-void Departure::handle() {}
+// void Departure::handle() {}
+void Departure::handle() {
+  /**
+  * MODIFICATIONS FAITES PAR MATTHIEU (24/09/2022)
+  * 
+  * Considérons la file d'attente WaitingQueue queue (à récupérer dans la bank)
+  * 
+  */
+  
+  /**
+  * Remarque: Dans Bank.cpp, une nouvelle fonction a été ajoutée
+  * Cette fonction renvoit la file d'attente "_queue" qui se trouve dans la bank
+  */
+  WaitingQueue queue = bank.queue();
+  
+  // Si la file d'attente est vide
+  if(queue.isEmpty()){
+    // Le caissier attend
+    // ...
+  }
+  // Sinon
+  else{
+    // Le premier client qui arrive est servi par le caissier
+    // ...
+    
+    // Remarque: Je pense qu'il faudra utiliser la fonction "handle()" de Arrival.cpp, quand elle sera prête.
+  }
+}
